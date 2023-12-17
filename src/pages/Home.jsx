46635ae70639f2e88380dcbe9ad51d59882902c2
akaeidalakib <akaeid786@gmail.com>
2023-12-17 20:43:49 +0600

    lazy load added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,15 @@
 import { useRef, useEffect, useState } from "react";
 import { Box } from "@chakra-ui/react";
 import { lazy, Suspense } from "react";
-// const Hero = lazy(() => import("./Hero"));
 import Hero from "./Hero";
+import Tutorial from "./Tutorial"
 const NftTypes = lazy(() => import("./NftTypes"));
 const Partners = lazy(() => import("./Partners"));
 const Features = lazy(() => import("./Features"));
-// const Shapeshifter = lazy(() => import("./Shapeshifter"));
 const Timeline = lazy(() => import("./Timeline"));
 const Footer = lazy(() => import("../components/Footer"));
 const About = lazy(() => import("./About"));
-const Tutorial = lazy(() => import("./Tutorial"));
+// const Tutorial = lazy(() => import("./Tutorial"));
 const Guild = lazy(() => import("./Guild"));
 
 import SectionLoading from "../components/SectionLoading";
